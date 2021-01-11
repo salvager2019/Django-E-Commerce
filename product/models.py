9073f5264816d52b703446eeed0a301bfc27a6cc
salvager2019 <s.salvager@gmail.com>
2021-01-11 21:54:28 +0800

    git commit -m "Salvager first Django Ecommmerce"

--- product/models.py
+++ product/models.py
@@ -38,7 +38,7 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
-    ##method to create a fake table field in read only model
+    ##method to create a fake table field in read only model. this is add github
     def image_tag(self):
         return mark_safe('<img src="{}" height="50"'.format(self.image.url))
     
