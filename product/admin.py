42148e5c6e18342beab66bcae671f1dc0b78b201
Your Name <s.salvager@gmail.com>
2021-01-13 17:33:50 +0800

    salvager

--- product/admin.py
+++ product/admin.py
@@ -1,18 +1,20 @@
 from django.contrib import admin
-from product.models import Category, Product,Images
+from product.models import Category, Product, Images
 
 # Register your models here.
-
-
-
 class CategoryAdmin(admin.ModelAdmin):
     list_display=['title','parent','status']
     list_filter=['status']
 
+class ProductImageInline(admin.TabularInline):
+    model = Images
+    extra = 5
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'status','image_tag']
     list_filter = ['category']
+    readonly_fields=('image_tag',)
+    inlines=[ProductImageInline]
 
 admin.site.register(Category,CategoryAdmin)
 admin.site.register(Product,ProductAdmin)
