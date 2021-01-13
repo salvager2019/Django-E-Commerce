4f06b0b4989fe8146ef679ad00f687cb8b262d7f
Your Name <s.salvager@gmail.com>
2021-01-13 15:35:38 +0800

    salvager

--- product/models.py
+++ product/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.safestring import mark_safe
 
 # Create your models here.
 class Category(models.Model):
@@ -44,3 +45,12 @@ class Product(models.Model):
         return mark_safe('<img src="{}" height="50"'.format(self.image.url))
     
     image_tag.short_description="Image"
+
+
+class Images(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50,blank=True)
+    image = models.ImageField(blank=True, upload_to='images/')
+
+    def __str__(self):
+        return self.title
